9de2afa299d91b0258d0b9d1a8d4ba6fefabde79
zhaojinjie <zhaojinjie96@gmail.com>
2019-06-12 17:51:45 +0800

    添加命令行输出cells的测试代码

---
+++ test/cellUpdateTest.py
@@ -0,0 +1,15 @@
+from src.cells import Cells
+import torch
+
+data = torch.zeros(10, 10).type(torch.LongTensor)
+data[0][2] = 1
+data[1][2] = 1
+data[2][2] = 1
+data[2][1] = 1
+data[1][0] = 1
+
+cells = Cells(data)
+
+for _ in range(10):
+    print(cells.matrix)
+    cells.update()
