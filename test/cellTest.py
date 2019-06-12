22d9225d2c6cd8de8b9e845b0169cbef54404687
zhaojinjie <zhaojinjie96@gmail.com>
2019-06-12 16:38:18 +0800

    修改测试 移除周围有三个细胞会存活的测试用例中周围有两个存活的细胞

--- test/cellTest.py
+++ test/cellTest.py
@@ -7,9 +7,9 @@ from src.cells import Cells
 class CellsTest(unittest.TestCase):
 
     def test_should_3_live(self):
-        tensor = torch.tensor([[0, 1, 0],
-                               [1, 0, 1],
-                               [0, 0, 0]])
+        tensor = torch.tensor([[0, 0, 1],
+                               [0, 0, 1],
+                               [1, 0, 0]])
         target = torch.tensor([[0, 0, 0],
                                [0, 1, 0],
                                [0, 0, 0]])
