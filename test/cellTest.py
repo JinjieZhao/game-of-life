74e9b44cca482942b55f23bc8d9e452e47c39e30
zhaojinjie <zhaojinjie96@gmail.com>
2019-06-12 16:46:19 +0800

    添加测试 周围少于两个存活细胞的细胞会死亡

--- test/cellTest.py
+++ test/cellTest.py
@@ -37,3 +37,17 @@ class CellsTest(unittest.TestCase):
         cells = Cells(tensor)
         cells.update()
         self.assertTrue(torch.equal(target, cells.matrix))
+
+    def test_should_less_die(self):
+        """
+        如果周围细胞数量小于2 细胞会死亡
+        """
+        tensor = torch.tensor([[0, 1, 0],
+                               [1, 0, 0],
+                               [0, 0, 0]])
+        target = torch.tensor([[0, 0, 0],
+                               [0, 0, 0],
+                               [0, 0, 0]])
+        cells = Cells(tensor)
+        cells.update()
+        self.assertTrue(torch.equal(target, cells.matrix))
