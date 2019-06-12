d9642ad90d44da55b5d3ff3bb38d0d30a2d027ef
zhaojinjie <zhaojinjie96@gmail.com>
2019-06-12 22:20:02 +0800

    添加测试 从rle_str 到tensor

---
+++ test/rleTest.py
@@ -0,0 +1,27 @@
+import torch
+import unittest
+
+import src.rle
+
+
+class relTest(unittest.TestCase):
+    def test_rel_decode(self):
+        rle_str = '7b2o$3bobo2bo$2o3b2o!'
+        h, w = 3, 10
+        target = torch.zeros(h, w)
+        ones = [
+            [0, 7],
+            [0, 8],
+            [1, 3],
+            [1, 5],
+            [1, 8],
+            [2, 0],
+            [2, 1],
+            [2, 5],
+            [2, 6]
+        ]
+
+        for i, j in ones:
+            target[i][j] = 1
+
+        self.assertTrue(torch.equal(target, src.rle.rle2tensor(rle_str)))
