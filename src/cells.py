c050127721d0f886bdb5a2e7bd352c01fd9424fe
zhaojinjie <zhaojinjie96@gmail.com>
2019-06-12 17:05:24 +0800

    重构代码 添加注释

--- src/cells.py
+++ src/cells.py
@@ -10,11 +10,18 @@ class Cells:
                                     [1., 1., 1.]]).view(1, 1, 3, 3)
 
     def conv(self):
+        """
+        对matrix 进行卷积操作，获取每个细胞周围存活的细胞数量矩阵
+        :return: 数量矩阵
+        """
+
+        # 把matrix 转成适合卷积的形式
         mat = self.matrix.view(1, 1, *self.shape).type(torch.FloatTensor)
+        # 进行卷积并转回原来的形式
         return torch.conv2d(mat, self.kernel, padding=1).view(*self.shape)
 
     def update(self):
-        live_mat_nearby = self.conv()
-        nearby_3_live = (live_mat_nearby == 3).type(torch.LongTensor)
-        nearby_2_live = (live_mat_nearby == 2).type(torch.LongTensor) * self.matrix
+        live_num_mat_nearby = self.conv()
+        nearby_3_live = (live_num_mat_nearby == 3).type(torch.LongTensor)
+        nearby_2_live = (live_num_mat_nearby == 2).type(torch.LongTensor) * self.matrix
         self.matrix = nearby_2_live + nearby_3_live
